08a62fb01d22c464ff46bee4de555647d2a1d1b9
maharanasubhendu3@gmail.com <maharanasubhendu3@gmail.com>
2023-11-01 06:46:48 +0530

    Remove comment line

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
                     <h6>${weatherItem.weather[0].description}</h6>
                 </div>`;
-    } else { // HTML for the other five day forecast card
+    } else {
         return `<li class="card">
                     <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
